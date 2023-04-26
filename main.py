f2239d4bf4ca85f2859966a22acbe29f5106fa28
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-26 21:31:52 +0100

    Write letters into new files

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 PLACEHOLDER = "[name]"
 
+#Get names from invited names
 with open("./Input/Names/invited_names.txt") as names_file:
-    names = names_file.readlines() #Get names from invited names
-    
+    names = names_file.readlines() 
+
+# Replace [name] with invited names  
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contains = letter_file.read()
     for name in names:
         stripped_name = name.strip()
-        new_letter = letter_contains.replace(PLACEHOLDER, stripped_name) # Replace [name] with invited names
+        new_letter = letter_contains.replace(PLACEHOLDER, stripped_name) 
 
         # Write the letters into new file
         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
